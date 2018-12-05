1744a50283e50ecc6957410163699b15fd83137c
tinashe banguwangu <banguwangu@gmail.com>
2018-12-05 14:34:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
 
-use System\core\File;
+/*use System\core\File;
 use System\core\Appication;
-/*
+
 Lets get the boostrapping class from the application system folder
-*/
+
 $file = File(__dir__);
 $app = Application($file);
 $app->getIstance();
+*/
