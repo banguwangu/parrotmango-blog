9907ed900b9f6491f7a5f3b20da750149fb4c315
tinashe banguwangu <banguwangu@gmail.com>
2018-12-06 11:12:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
 
+#lets require the config file from our root folder
+require 'config.php';
 
-/*use System\core\File;
-use System\core\Appication;
+#the file class generats directory path and help us with loading the required files
+require APPPATH.s.'System'.s.'core'.s.'File.php';
+#bootstrap file is important in generating the request and processing all request
+require APPPATH.s.'System'.s.'core'.s.'Bootstrap.php';
 
-Lets get the boostrapping class from the application system folder
+#this is required to handle our namespacing
+use System\core\File;
+use System\core\Bootstrap;
 
-$file = File(__dir__);
-$app = Application($file);
-$app->getIstance();
-*/
+
+
+$file = new File(APPPATH);
+#now lets get the current file instance
+$app = Bootstrap::init_instance($file);
+
+#finally we initialize the application
+$app->init();
